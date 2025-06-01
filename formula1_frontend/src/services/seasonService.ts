77a79490d8b2709ccfbb8474ae5c0075094e5962
iqose <ext_irena.qose@spark.net>
2025-06-01 17:09:16 +0200

    Update env url to get it from env

--- formula1_frontend/src/services/seasonService.ts
+++ formula1_frontend/src/services/seasonService.ts
@@ -1,25 +1,9 @@
 
-import type { Season, SeasonResultsResponse } from '../types/season';
-
-const API_BASE_URL = 'http://localhost:3000';
-
-export const fetchSeasons = async (): Promise<Season[]> => {
-  try {
-    const response = await fetch(`${API_BASE_URL}/seasons`);
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Error fetching seasons:', error);
-    throw error;
-  }
-};
+import type { SeasonResultsResponse } from '../types/season';
 
 export const fetchSeasonResults = async (): Promise<SeasonResultsResponse> => {
   try {
-    const response = await fetch(`${API_BASE_URL}/seasons/results`);
+    const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/seasons/results`);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
